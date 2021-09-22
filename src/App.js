f0fa3075006dc30f049bec5e17cc488d9e9f189c
Ishita Choudhary <69318862+chrysanthemum027@users.noreply.github.com>
2021-09-22 16:23:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          column number 1
+        </div>
+        <div className="col-6">
+          column number 1
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
